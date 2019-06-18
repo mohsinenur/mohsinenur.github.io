bf5994d128c0f75b944bf5f263c3bdd3073d1539
Mohsin <follow.mohsin@gmail.com>
2019-06-18 10:55:34 +0600

    added new js

--- js/jquery.typer.js
+++ js/jquery.typer.js
@@ -10,8 +10,8 @@
     $.fn.typer = function(text, options){
         options = $.extend({}, {
             char: '',
-            delay: 2000,
-            duration: 600,
+            delay: 5000,
+            duration: 800,
             endless: true,
             onType: $.noop,
             afterAll: $.noop,
